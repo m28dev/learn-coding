2a53f2f8adbbbdfb292c6907045852613c553ead
m28dev <29790650+m28dev@users.noreply.github.com>
2025-08-24 15:52:00 +0900

    LeetCode 141, 20

---
+++ leetcode/linked-list/141-linked-list-cycle.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    const seen = new Set();
+
+    while (head != null) {
+        if(seen.has(head)) return true;
+        seen.add(head);
+        head = head.next;
+    }
+
+    return false;
+}
+
+type ListNode = {
+    val: number;
+    next: ListNode | null;
+};
